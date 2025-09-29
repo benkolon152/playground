98c609bea8669b4470cbeac387ada2f3c8733d92
Benkolon152 <v.benasz@gmail.com>
2025-09-29 08:47:31 +0200

    Blur

--- react-app/src/pages/SimplePage.jsx
+++ react-app/src/pages/SimplePage.jsx
@@ -14,7 +14,11 @@ export default class SimplePage extends React.Component {
         e.prevetDefault()
 
         //console.log('handleFormsubmit', e)
-        console.log('email', this.setEmailww)
+        console.log('email', this.state.email)
+    }
+
+    handleEmailBlur = e =>{
+        console.log('handleblur', e)
     }
 
     render() {
@@ -23,7 +27,10 @@ export default class SimplePage extends React.Component {
             <h2>Simple Email Form</h2>
                 <form onSubmit={this.handleFormSubmit}>
                     <label htmlFor="simpleEmail">Email address:</label>
-                    <input type="email" id="simpleEmail" name="simpleEmail" placeholder="name@example.com" required autoComplete="on" onChange={e => this.setEmail(e.target?.value )}/>
+                    <input type="email" id="simpleEmail" name="simpleEmail" placeholder="name@example.com" required autoComplete="on" 
+                        onChange={e => this.setEmail(e.target?.value)}
+                        onBlur={this.handleEmailBlur}
+                        />
                     <button type="submit">Submit</button>
                     <input type="reset" value={"Reset"}/>
                 </form>
