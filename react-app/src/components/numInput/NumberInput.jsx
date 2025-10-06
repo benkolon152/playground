591d9181e491ef963b4bd1378f4cdf3ed8c45d19
Benkolon152 <v.benasz@gmail.com>
2025-10-06 08:59:15 +0200

    bug fix

--- react-app/src/components/numInput/NumberInput.jsx
+++ react-app/src/components/numInput/NumberInput.jsx
@@ -1,11 +1,16 @@
 import React, { useState} from "react";
 const NumInput = props => {
     const [number, setNumber] = useState(42)
-    
+    const [minValue, setMinValue] = useState(0)
+    const [maxValue, setMaxValue] = useState(100)
 
     const handleNumberChange = e =>{
         //console.log('numchange', e)
-        setNumber(e.target.value)
+        let newNumberValue = +e.target.value
+        if (newNumberValue<minValue) newNumberValue = minValue
+        if (newNumberValue>maxValue) newNumberValue = maxValue
+
+        setNumber(newNumberValue)
     }
 
     return(
